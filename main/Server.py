543925b2d7604d721284bc407d849cadefda484b
bippity <alex.waang@yahoo.com>
2019-12-02 16:45:59 -0600

    Added getFiles() into Server
    Added basic function to get a list of files in a directory

---
+++ main/Server.py
@@ -0,0 +1,16 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
+
+data_folder = Path("files")
+
+# Returns a list of files inside the "files" directory
+def listFiles():
+    print("Getting files in: " + str(data_folder))
+    files = [f for f in listdir(data_folder) if isfile(join(data_folder, f))]
+    return files
+
+
+print(listFiles())
